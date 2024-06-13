b438b07b31c6e99cc4f667e964532cacef45e232
Ali Alipoor <alialipoordev@gmail.com>
2024-06-13 12:13:34 +0330

    add homepage & styles

---
+++ src/constant/text.ts
@@ -0,0 +1,4 @@
+export const text1: string =
+  "This is a full build that covers the full authentication process from login, Register, sending emails (for activating amount,reset password and password change notice), forgot password, reset password,advanced form validation,protected routes, session manipulation...";
+export const text2: string =
+  "In this build we used React js, Next js, Mongodb, Mongoose, Typescript, Next auth,React-hook-form, Zod,Axios, Nodemailer, Smtp service, Gmail smtp, Axios, React-toastify, Zxcvbn, Handlebars, BcryptJs...";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,71 @@
+import { text1, text2 } from "@/constant/text";
 import { NextPageContext } from "next";
 import { getSession, useSession } from "next-auth/react";
 import { signIn, signOut } from "next-auth/react";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function Home() {
   const { data: session } = useSession();
   console.log(session);
 
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      {session ? (
-        <div className="flex flex-col gap-1 items-center">
-          <h2>{session.user?.name}</h2>
-          <img
-            src={session.user?.image as string}
-            alt="profile image"
-            className="w-32 h-32 rounded-full"
-          />
-          <h4>{session.user?.email}</h4>
-          <span>
-            Provider: <b>{session.user?.provider}</b>
-          </span>
-          <button onClick={() => signOut()}>sign out</button>
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <div className="container mx-auto">
+        <div className="border border-white relative flex flex-col w-full rounded-lg">
+          <div className="flex flex-wrap justify-center items-center">
+            <div className="w-full text-right">
+              <div className="py-6 px-3">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700
+                   text-md uppercase font-bold px-8 py-2 rounded-md
+                    sm:mr-2 mb-1 ease-linear tra transition-all duration-150"
+                  onClick={() => signOut()}
+                >
+                  Logout
+                </button>
+              </div>
+            </div>
+            <div className="w-full flex justify-center">
+              <img
+                src={session?.user?.image!}
+                alt={`${session?.user?.name} image`}
+                className="rounded-full h-40 w-40"
+              />
+            </div>
+            <div className="text-center mt-12">
+              <h3 className="text-4xl font-semibold mb-2">
+                {session?.user?.name}
+              </h3>
+              <div className="text-sm mb-2 font-bold">
+                {session?.user?.email}
+              </div>
+              <div className="mb-2 mt-10">
+                You logged in using &nbsp;
+                <span
+                  className="capitalize bg-blue-400 text-white
+                 px-4 py-1 ml-2 font-bold italic text-lg rounded-md"
+                >
+                  {session?.user?.provider}
+                </span>
+              </div>
+            </div>
+            <div className="mt-10 py-10 border-t text-center">
+              <div className="flex flex-wrap justify-center">
+                <div className="w-full px-4">
+                  <p className="mb-4 text-sm">{text1}</p>
+                  <p className="font-bold text-xs">{text2}</p>
+                  <div className="mt-6 flex items-center justify-center gap-2">
+                    Source code here : &nbsp;
+                    <a href="http://" target="_blank" rel="noopener noreferrer" className="text-4xl">
+                      <AiFillGithub />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      ) : (
-        <button onClick={() => signIn()}>sign in</button>
-      )}
+      </div>
     </div>
   );
 }
