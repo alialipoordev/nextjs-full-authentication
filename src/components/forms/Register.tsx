eefd9e39bd62272449de42e1157ecde8b8d48c45
Ali Alipoor <alialipoordev@gmail.com>
2024-06-14 14:34:49 +0330

    add validation for lastname, email, phone

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -4,6 +4,9 @@ import { CiUser } from "react-icons/ci";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { FiMail } from "react-icons/fi";
+import { BsTelephone } from "react-icons/bs";
+import validator from "validator";
 
 interface IRegisterFormProps {}
 
@@ -13,6 +16,15 @@ const FormSchema = z.object({
     .min(2, "First name must be at least 2 characters")
     .max(32, "First name must be less than 32 characters")
     .regex(new RegExp("^[a-zA-Z]+$"), "No special characters allowed."),
+  last_name: z
+    .string()
+    .min(2, "Last name must be at least 2 characters")
+    .max(32, "Last name must be less than 32 characters")
+    .regex(new RegExp("^[a-zA-Z]+$"), "No special characters allowed."),
+  email: z.string().email("Please enter a valid address."),
+  phone: z.string().refine(validator.isMobilePhone, {
+    message: "Please enter a valid phone number",
+  }),
 });
 
 type FormSchemaType = z.infer<typeof FormSchema>;
@@ -42,8 +54,38 @@ const RegisterForm: React.FunctionComponent<IRegisterFormProps> = (props) => {
           error={errors?.first_name?.message}
           disabled={isSubmitting}
         />
-        <button type="submit">Submit</button>
+        <Input
+          name="last_name"
+          label="Last name"
+          type="text"
+          icon={<CiUser />}
+          placeholder="example"
+          register={register}
+          error={errors?.last_name?.message}
+          disabled={isSubmitting}
+        />
       </div>
+      <Input
+        name="email"
+        label="Email"
+        type="text"
+        icon={<FiMail />}
+        placeholder="example"
+        register={register}
+        error={errors?.email?.message}
+        disabled={isSubmitting}
+      />
+      <Input
+        name="phone"
+        label="Phone number"
+        type="text"
+        icon={<BsTelephone />}
+        placeholder="+(xxx) xxx-xx-xx"
+        register={register}
+        error={errors?.phone?.message}
+        disabled={isSubmitting}
+      />
+      <button type="submit">Submit</button>
     </form>
   );
 };
