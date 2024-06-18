8f9ccdf204bf7b81f083b07632bb43b5af727a5f
Ali Alipoor <alialipoordev@gmail.com>
2024-06-18 16:15:52 +0330

    feat: custom provider logic

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,4 @@
 import NextAuth, { Account, Profile, User } from "next-auth";
-import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
@@ -14,10 +13,6 @@ import { compare } from "bcryptjs";
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise) as Adapter,
   providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID as string,
-      clientSecret: process.env.FACEBOOK_SECRET as string,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,18 +1,21 @@
 import Background from "@/components/backgrounds/Background";
+import SocialButton from "@/components/buttons/SocialButton";
 import LoginForm from "@/components/forms/Login";
 import RegisterForm from "@/components/forms/Register";
 import { NextPageContext } from "next";
-import { getCsrfToken } from "next-auth/react";
+import { getCsrfToken, getProviders } from "next-auth/react";
 import React from "react";
 
 function auth({
   tab,
   callbackUrl,
   csrfToken,
+  providers,
 }: {
   tab: string;
   callbackUrl: string;
   csrfToken: string;
+  providers: any;
 }) {
   return (
     <div className="w-full flex items-center justify-center">
@@ -21,13 +24,35 @@ function auth({
         <div
           className="w-full sm:w-5/6 md:w-2/3 
           lg:w-1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white 
-          flex items-center justify-center"
+          flex flex-col items-center justify-center"
         >
           {tab == "signin" ? (
             <LoginForm callbackUrl={callbackUrl} csrfToken={csrfToken} />
           ) : (
             <RegisterForm />
           )}
+          <div className="w-full flex items-center justify-between px-12">
+            <div className="w-full h-[1px] bg-gray-300" />
+            <span className="text-sm uppercase mx-6 text-gray-400">Or</span>
+            <div className="w-full h-[1px] bg-gray-300" />
+          </div>
+          <div className="mt-3 grid grid-cols-2 gap-4">
+            {providers.map((provider: any) => {
+              if (provider.name == "Credentials") return;
+              return (
+                <SocialButton
+                  key={provider.id}
+                  id={provider.id}
+                  csrfToken={csrfToken}
+                  text={
+                    tab == "signup"
+                      ? `Sign up with ${provider.name}`
+                      : `Sign in with ${provider.name}`
+                  }
+                />
+              );
+            })}
+          </div>
         </div>
         {/* Background */}
         <Background
@@ -51,7 +76,13 @@ export async function getServerSideProps(context: NextPageContext) {
 
   const csrfToken = await getCsrfToken(context);
 
+  const providers = await getProviders();
   return {
-    props: { tab: JSON.parse(JSON.stringify(tab)), callbackUrl, csrfToken },
+    props: {
+      tab: JSON.parse(JSON.stringify(tab)),
+      callbackUrl,
+      csrfToken,
+      providers: Object.values(providers!),
+    },
   };
 }
